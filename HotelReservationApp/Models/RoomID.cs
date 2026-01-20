4fc97c1300ad9148572bd6cdb041dc4038bc7464
Aevael <98851230+Aevael@users.noreply.github.com>
2026-01-19 18:06:00 -0600

    Repository creation and initial upload.

---
+++ HotelReservationApp/Models/RoomID.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationApp.Models
+{
+    public class RoomID
+    {
+        public int FloorNumber { get; }
+        public int RoomrNumber { get; }
+    }
+
+    public override bool Equals(object? obj)
+        {
+            return obj is RoomID roomID && FloorNumber == roomID.FloorNumber && RoomrNumber == roomID.RoomrNumber;
+        }
+    }
+}
